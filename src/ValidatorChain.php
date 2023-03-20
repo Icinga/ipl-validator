8afa9bc0c30a1e70ee4545ff85e1d623b07d98dd
Johannes Meyer <johannes.meyer@icinga.com>
2023-03-20 16:51:40 +0100

    Revert "ValidatorChain: Don't ask ordinary validators if they accept empty values"
    This reverts commit 3a44e261888f2de17ad4c9a07895d01413d82636.

--- src/ValidatorChain.php
+++ src/ValidatorChain.php
@@ -140,7 +140,7 @@ class ValidatorChain implements Countable, IteratorAggregate, Validator
 
                 $validator = $this->createValidator($name, $validator);
 
-                if ($validateEmpty !== null && $validator instanceof BaseValidator) {
+                if ($validateEmpty !== null) {
                     $validator->setValidateEmpty($validateEmpty);
                 }
             }
@@ -276,11 +276,7 @@ class ValidatorChain implements Countable, IteratorAggregate, Validator
         $valid = true;
 
         foreach ($this as $validator) {
-            if (empty($value) && $validator instanceof BaseValidator && ! $validator->validateEmpty()) {
-                continue;
-            }
-
-            if ($validator->isValid($value)) {
+            if ((empty($value) && ! $validator->validateEmpty()) || $validator->isValid($value)) {
                 continue;
             }
 
